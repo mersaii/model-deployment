f425b5e5c8652adfb4237061ee4b9f46a078a534
Mercy Ade-Ige <mercy.ade-ige@kibo.school>
2023-12-09 10:46:51 +0100

    model complete

---
+++ app.py
@@ -0,0 +1,21 @@
+from flask import Flask, request, jsonify
+import joblib
+from sklearn.feature_extraction.text import CountVectorizer
+
+app = Flask(__name__)
+
+# Load the saved model
+model = joblib.load('C:/Users/mercy/IDS/model-deployment/sentiment_model.pkl')
+vectorizer = joblib.load('C:/Users/mercy/IDS/model-deployment/vectorizer.pkl')
+
+# create a route that manages user request and does sentiment prediction
+@app.route('/predict', methods=['POST'])
+def predict():
+    data = request.get_json()
+    text = data['text']
+    vectorized_text = vectorizer.transform([text])
+    prediction = model.predict(vectorized_text)[0]
+    return jsonify({'sentiment': str(prediction)})
+
+if __name__ == '__main__':
+    app.run()
